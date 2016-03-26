2dd94acd91dc1e63d203c244b17dd9ce80c161c6
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-03-26 12:11:01 -0700

    Setting up modules and some utils

---
+++ JavaScript/Trees/binarySearchTree.js
@@ -0,0 +1,13 @@
+var node = require('./treeNode.js');
+var inherits = require('../Utils/utils.js');
+
+var binarySearchTree = function() {};
+inherits(binarySearchTree, node);
+
+binarySearchTree.prototype = {
+    insert: '',
+    remove: '',
+    contains: ''
+};
+
+module.exports = binarySearchTree;
--- JavaScript/Trees/treeNode.js
+++ JavaScript/Trees/treeNode.js
@@ -1,6 +1,10 @@
-function Node(val, parent) {
+'use strict';
+
+var Node = function(val, parent) {
     this.parent = parent || null;
     this.val = val || null;
     this.leftChild = null;
     this.rightChild = null;
 }
+
+module.exports = Node;
