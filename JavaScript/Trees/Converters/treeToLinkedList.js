ef27d65594a38badbaa71662df9a7e7195e5c974
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-03-25 22:13:09 -0700

    Create treeToLinkedList.js

---
+++ JavaScript/Trees/Converters/treeToLinkedList.js
@@ -0,0 +1,33 @@
+function pushRight(tree, rightChild) {
+    if(tree.rightChild){
+        pushRight(tree.rightChild);
+    }
+    tree.rightChild = rightChild;
+}
+
+function flatten(tree){
+    var left = null;
+    var right = null;
+
+    if(tree.leftChild){
+        left = flatten(tree.leftChild);
+        tree.leftChild = null;
+    }
+
+    if(tree.rightChild){
+        right = flatten(tree.rightChild);
+        tree.rightChild = null;
+    }
+
+    if(left){
+        pushRight(left, tree);
+    } else {
+        left = tree;
+    }
+
+    if(right){
+        pushRight(left, right);
+    }
+
+    return left;
+}
