7e3137423b20a465e68191265254ef49512700ff
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-07-26 20:27:32 -0700

    thinking algo through

---
+++ JavaScript/src/Graphs/Converters/adjListToIncidenceMatrix.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function adjListToIncidenceMatrix(matrix) {
+
+}
+
+module.exports = adjListToIncidenceMatrix;
--- JavaScript/tests/adjListToIncidenceMatrix_spec.js
+++ JavaScript/tests/adjListToIncidenceMatrix_spec.js
@@ -7,7 +7,7 @@ describe('Utils', function () {
         var list = {
             0: [1, 2],
             1: [0, 3],
-            2: [0],
+            2: [0, 3],
             3: [1, 2]
         }
             ;
