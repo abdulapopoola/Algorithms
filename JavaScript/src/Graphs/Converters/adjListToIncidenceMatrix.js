4aaa0a5a33f6ea5ac69fa7123ca23b7587544f1e
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-08-03 22:22:30 -0700

    fixed incident matrix

--- JavaScript/src/Graphs/Converters/adjListToIncidenceMatrix.js
+++ JavaScript/src/Graphs/Converters/adjListToIncidenceMatrix.js
@@ -5,20 +5,23 @@ function adjListToIncidenceMatrix(matrix) {
     var matrixLen = vertices.length;
     var edgesMap = {};
     var incidentMatrix = [];
+    var edgeCounter = 0;
     for (var i = 0; i < matrixLen; i++) {
         var vertix = vertices[i];
         var edgeList = matrix[vertix];
-        var edgeMatrix = [];
-        var edgeCounter = 0;
+        var edgeMatrix = Array.apply(null, new Array(matrixLen)).map(Number.prototype.valueOf, 0);
         for (var j = 0, len = edgeList.length; j < len; j++) {
             var edgeKey = getEdgeKey(vertix, edgeList[j]);
             var edgeNumber = edgesMap[edgeKey];
-            if (edgeNumber) {
-                //find a way to link edgenumbers to edgeMatrix values
-                //edgeMatrix[j]
+            if (edgeNumber == null) {
+                edgeNumber = edgeCounter++;
+                edgesMap[edgeKey] = edgeNumber;
             }
+            edgeMatrix[edgeNumber] = 1;
         }
+        incidentMatrix.push(edgeMatrix);
     }
+    return incidentMatrix;
 }
 
 function getEdgeKey(vertix1, vertix2) {
