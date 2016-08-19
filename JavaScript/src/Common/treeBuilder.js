21107241beb333a39ae268dfacb197f3f5e09d3b
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-08-19 07:40:01 -0700

    started on vertexCover

---
+++ JavaScript/src/Common/treeBuilder.js
@@ -0,0 +1,2 @@
+//Use a tree like the expression parsing trees for languages?
+//TODO: Is this even needed?
---
+++ JavaScript/tests/vertexCover_spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var TreeNode = require('../src/Common/treeNode');
+var evaluate = require('../src/Graphs/vertexCover');
+
+describe('VertexCover', function () {
+    it('should calculate the vertexCover', function () {
+        expect(false).toEqual(true);
+    });
+});
