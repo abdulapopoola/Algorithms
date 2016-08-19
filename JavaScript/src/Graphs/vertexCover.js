8d7d7ad0fffb57ac64126c09061b3d36975ebf38
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-08-19 07:42:03 -0700

    algo

--- JavaScript/src/Graphs/vertexCover.js
+++ JavaScript/src/Graphs/vertexCover.js
@@ -1,7 +1,10 @@
 'use strict';
 
 function vertexCover(graph) {
-
+    //start from each vertex
+    // remove all vertices it is linked to 
+    //if no more vertices, return set
+    //append to list
 }
 
 module.exports = vertexCover;
